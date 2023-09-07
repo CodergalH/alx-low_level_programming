5957593500895debcb8095d40bf061b54a8013b6
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-07 14:02:40 -0700

    First c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,9 @@
  * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
  /* puts - Prints string */
- puts("\"Programming is like building a multilingual puzzle \n");
- return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
