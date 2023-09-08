c4913904402168f5c2f3f28e517fe535653e508f
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-07 20:11:16 -0700

    Change asembler syntax

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (Success)
+ *
+ * Return: Prints message to standard error
+ */
+
+int main(void)
+{
+putchar(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+}
