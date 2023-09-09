437f4ac6ba3b0fde3ae8df9257f4080deaee7f39
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-08 20:57:04 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return - 0 (Success)
+ *
+ * Return: Print out numbers
+ */
+
+int main(void)
+{
+int a = 1;
+
+do {
+printf("%d", a);
+a++;
+} while (a < 9);
+printf("\n");
+
+return (0);
+}
