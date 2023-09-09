18d8f3b5a81cdb0a5845d653d1ca9f41449d3765
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-08 19:14:32 -0700

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return - 0 (Success)
+ * Return: Returns the alphabets in lowercase
+ */
+
+int main(void)
+{
+int i;
+
+i = 97;
+do {
+putchar(i);
+i++;
+putchar(i'\,');
+} while (i < 123);
+return (0);
+}
