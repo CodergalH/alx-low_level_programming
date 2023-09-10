8f1824b8e8480856166ef2435373d4d001979a2b
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-10 09:03:43 -0700

    Four combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints out all possible
- * combinations of 2 numbers between 0 and 100
+ * combinations of 2 single numbers
  *
  * Return: 0 success
  */
@@ -13,23 +13,35 @@ int num1, num2;
 
 num1 = 0;
 num2 = 0;
+num3 = 0;
+num4 = 0;
+
 do {
 do {
 num2 = num1 + 1;
 do {
+num3 = num1;
+do {
+num4 = num2 + 1;
+do {
 putchar(num1 + '0');
-putchar(' ');
 putchar(num2 + '0');
-if (num1 == "98" && num2 == "99")
+putchar(' ');
+putchar(num3 + '0');
+putchar(num4 + '0');
+if (num1 == 9 && num2 == 8 && (num3 && num4 == 9))
 break;
 putchar(',');
-putchar(' ');
+num4++
+} while (num4 != 10);
+num3++;
+} while (num3 != 10);
 num2++;
-} while (num2 != "100");
+} while (num2 != 9);
 num1++;
-} while (num1 != "99");
+} while (num1 != 10);
 break;
-} while (num1 != "98"  && num2 != "99");
+} while ((num1 != 9 && num2 != 8) && (num3 && num4 != 9));
 putchar('\n');
 
 return (0);
