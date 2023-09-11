048086eddcd005a4efd69f64eb6aac4f02310a4b
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-11 09:41:13 -0700

    Three combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,40 +9,26 @@
 
 int main(void)
 {
-int num1, num2, num3, num4;
+int num1, num2;
 
-num1 = 0;
-num2 = 0;
-num3 = 0;
-num4 = 0;
-
-do {
-do {
-num2 = num1 + 1;
-do {
-num3 = num1;
-num4 = num2 + 1;
-do {
-do {
-putchar(num1 + '0');
-putchar(num2 + '0');
+for (num1 = 0; num1 <= 98; num1++)
+{
+for (num2 = num1 + 1; num2 <= 99; num2++)
+{
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
 putchar(' ');
-putchar(num3 + '0');
-putchar(num4 + '0');
-if (num1 == 9 && num2 == 8 && (num3 && num4 == 9))
-break;
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+
+if (num1 == 98 && num2 == 99)
+continue;
+
 putchar(',');
 putchar(' ');
-num4++;
-} while (num4 != 10);
-num3++;
-} while (num3 != 10);
-num2++;
-} while (num2 != 9);
-num1++;
-} while (num1 != 10);
-break;
-} while ((num1 != 9 && num2 != 8) && (num3 && num4 != 9));
+}
+}
+
 putchar('\n');
 
 return (0);
