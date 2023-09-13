a4ffafe572da343053588e8895457b651d696740
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-12 21:57:28 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - print times table
+ *
+ * Return: 0 success
+ */
+
+void times_table(void)
+{
+	int x, y, z, u, d;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+
+			if (z > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
+
+				putchar(44);
+				putchar(32);
+				putchar(d + '0');
+				putchar(u + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					putchar(44);
+					putchar(32);
+					putchar(32);
+				}
+
+				putchar(z + '0');
+			}
+		}
+
+		putchar('\n');
+	}
+}
