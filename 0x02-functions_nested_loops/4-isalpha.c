93dd4fb6c5cdb09a6b786c265b26138c11d22e0b
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-12 18:46:10 -0700

    Check if alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isalpha - check if arguement passed is an alphabet
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if alphabet
+ * 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+
+while ((c >= 97 && c <= 122) || (c >= 65 && <= 90)
+{
+return (1);
+}
+
+return (0);
+}
