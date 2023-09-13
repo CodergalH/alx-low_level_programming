842158bf895a0bf94158483408a2205f2e974254
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-12 21:11:27 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of the day
+ *
+ * Return: 0 success
+ */
+
+void jack_bauer(void)
+{
+
+int i, j, k, l;
+
+for (i = 0; i <= 2; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+for (k = 0; j <= 5, j++)
+{
+for (l = 0; k <= 9; k++)
+{
+_putchar(i + '0');
+_putchar(j + '0');
+_putchar(':');
+_putchar(k + '0');
+_putchar(l + '0');
+_putchar('\n');
+}
+}
+}
+}
+
+}
