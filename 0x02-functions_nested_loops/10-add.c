439609d2afaa1210dbf328dd4c7e15027a68fcd5
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-13 04:53:21 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two intergers
+ *
+ * @a: number to be treated
+ * @b: number to be treated
+ *
+ * Return: value of a + b (success)
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
