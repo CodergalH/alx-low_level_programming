da2ebb75cf318a4b05683d19d11756596544cc4a
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-12 20:24:43 -0700

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,15 +14,10 @@ int print_last_digit(int a)
 
 if (a < 0)
 {
-return ((a % 10) * -1);  
-_putchar((a % 10) * -1 + '0');
+a = a * -1;
 }
-else
-{
-return (a % 10);
 _putchar(a % 10 + '0');
-}
 
-return (a);
+return (a % 10);
 
 }
