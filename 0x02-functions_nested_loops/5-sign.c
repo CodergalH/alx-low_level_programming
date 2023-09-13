9e4e0a8841c0cdfa865817dd2afb6a723b8d0dad
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-12 19:14:19 -0700

    Chech sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - prints sign of a number
+ *
+ * @n: value to be checked
+ *
+ * Return: 1 if n is greater than 0
+ * 0 if n is 0
+ * -1 if n is less than 1
+ *
+ */
+
+int print_sign(int n)
+{
+
+if (n == 0)
+{
+_putchar(n + '0');
+return (0);
+}
+
+if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+
+return (0);
+
+}
