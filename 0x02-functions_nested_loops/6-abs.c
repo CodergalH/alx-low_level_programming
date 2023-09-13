66685159e7449e7b759edfe98e764bd5f99e11dc
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-12 19:42:12 -0700

    Absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of a number
+ *
+ * @int: arguement
+ *
+ * Return: 0 success
+ *
+ */
+
+int _abs(int)
+{
+int j = int;
+if (j < 0)
+_putchar (j * -1);
+else
+_putchar(j);
+
+return (0);
+
+}
