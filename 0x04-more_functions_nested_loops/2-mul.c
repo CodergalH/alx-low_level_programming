139a2fac4d3fb00db2557976b7ff1cb6de35cf1a
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-15 13:33:04 -0700

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - check if the int c is uppercase
+ * @a: input
+ * @b: input
+ *
+ * Return: 1 (success) or 0 (otherwise)
+ */
+
+int mul(int a, int b)
+{
+int mux;
+
+mux = a * b;
+
+return (mux);
+
+}
