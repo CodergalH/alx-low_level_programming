b51235a965a538a5ae6e1e7a64092a1c414be6a9
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-15 18:43:11 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * fizz_buzz - prints numbers from 1 to 100
+ * for multiples of 3 prints fizz
+ * for multiples of 5 prints buzz
+ * for multiples of 3 and 5 prints fizzbuzz
+ *
+ */
+
+void fizz_buzz(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+if (i % 15 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
+printf(" ");
+}
+printf("\n");
+}
