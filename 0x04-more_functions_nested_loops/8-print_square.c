29070ccdb3145d0ce61a242937c223128b119714
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-15 18:01:12 -0700

    Print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal -  print backslashes equal to value of int n
+ *
+ * @size: interger
+ */
+
+void print_square(int size)
+{
+int i, j;
+
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar(35);
+}
+
+if (i == (n - 1))
+continue;
+_putchar(10);
+}
+}
+_putchar(10);
+
+}
