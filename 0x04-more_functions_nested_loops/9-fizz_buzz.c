e95b0f6e60222a59532f696b8db5e93c5a9f0767
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-15 18:58:29 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,6 +7,7 @@
  * for multiples of 5 prints buzz
  * for multiples of 3 and 5 prints fizzbuzz
  *
+ * Return: Always 0.
  */
 
 int main(void)
@@ -15,25 +16,33 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
+
 if (i % 15 == 0)
 {
 printf("FizzBuzz");
 }
+
 else if (i % 3 == 0)
 {
 printf("Fizz");
 }
+
 else if (i % 5 == 0)
 {
 printf("Buzz");
 }
+
 else
 {
 printf("%d", i);
 }
+
 if (i == 100)
 continue;
 printf(" ");
 }
+
 printf("\n");
+
+return (0);
 }
