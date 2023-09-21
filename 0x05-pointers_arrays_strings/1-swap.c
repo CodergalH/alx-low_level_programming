9a86ff1f4453a71b89b972bc2a31e4d364bd6cc8
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-21 03:55:25 -0700

    Swap two intergers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - swap two intergers
+ *@a: first interger
+ *@b: second interger
+ */
+
+void swap_int(int *a, int *b)
+{
+int *pnt = *a;
+*a = *b;
+*b = pnt;
+ 
+}
