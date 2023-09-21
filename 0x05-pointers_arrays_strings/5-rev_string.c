95acc7050c9d4f69ff78acbe594ce26c5bcbaad4
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-21 04:43:28 -0700

    Print string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - print string in reverse
+ *@s: string treated
+ *
+ */
+
+void rev_string(char *s)
+{
+int counter, add;
+
+add = 0;
+for (counter = 0; s[counter] != '\0'; counter++)
+add++;
+
+for (counter = add - 1; counter >= 0; counter--)
+{
+_putchar(s[counter]);
+}
+
+}
