0ca30deba283d5f41ce6eff305887d16abfb1c5d
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-09-28 03:28:35 -0700

    advanced tasks

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,10 +1,11 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
-* print_number - prints an integer
-*@n:integer to be printed
-*Return: nothing
-*/
+ * print_number - prints an integer
+ *@n:integer to be printed
+ *Return: nothing
+ */
 
 void print_number(int n)
 {
