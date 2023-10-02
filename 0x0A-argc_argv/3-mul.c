fd895cb83ab3f4a03dc104a1e40e2241f82868bb
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-10-02 15:50:18 -0700

    multiply numbers passed as arguements

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - multiply two numbers and print value
+ *
+ * @argc: no of arguement
+ * @argv: pointer to array of pointer to arguement string
+ * Return: (0) success (1) failed
+ */
+
+int main(int argc, char *argv[])
+{
+int i, j, mul;
+
+if (argc == 3)
+{
+i = atoi(argv[1]);
+j = atoi(argv[2]);
+mul = i * j;
+
+printf("%d\n", mul);
+}
+else
+{
+printf("Error\n");
+
+return (1);
+}
+
+return (0);
+}
