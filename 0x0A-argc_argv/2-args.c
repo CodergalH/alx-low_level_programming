1c89cace9d26d54a33a293c65c3d738684a7ae28
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-10-02 15:17:51 -0700

    Print all argument

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print all arguement passed
+ * 
+ * @argc: no of arguement
+ * @argv: pointer to array of pointer
+ * Return: (0) Success
+ */
+
+int main(int argc, char *argv[])
+{
+int count;
+  
+if (argc >= 1)
+{
+for (count = 0; count < argc; count++)
+{
+printf("%s\n", argv[count]);
+}
+}
+
+return (0);
+}
