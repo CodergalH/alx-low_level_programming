f800e49b5895f8e874c111e523a1931827561efa
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-10-02 14:35:02 -0700

    Print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print program name followed by \n
+ *
+ * @argc: argument count
+ * @argv: pointer to array of string of pointer
+ * Return: (0) Success
+ */
+
+int main(int argc, char *argv[])
+{
+
+int count = 0;
+
+printf("%s\n", argv[count]);
+
+return (0);
+}
