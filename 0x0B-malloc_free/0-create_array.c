dee4491c750a39e442c09d0761d752df742a7907
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-10-03 12:31:16 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,7 @@
 char *create_array(unsigned int size, char c)
 {
 char *str;
+unsigned int count;
 
 if (size == 0)
 {
@@ -20,7 +21,6 @@ return (NULL);
 
 str = malloc(sizeof(char) * size);
 
-unsigned int count;
 for (count = 0 ; count <= size - 1 ; count++)
 str[count] = c;
 
