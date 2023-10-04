efa1e7d93e281d5e21188c5ebc5ea9b11e8ec623
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-10-04 00:07:43 -0700

    concat string"

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - merge strings given as parameter
+ *
+ * @s1: string
+ * @s2: second string
+ * Return: pointer to merged string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+int size, size1, size2;
+char *str1;
+
+if (str == NULL)
+{
+return (NULL);
+}
+
+for (size = 0, size1 = 0; s1[size] != '\0';)
+{
+size += 1;
+size1 += 1;
+}
+for (size2 = 0; s2[size2] != '\0';)
+{
+size += 1;
+size2 += 1;
+}
+
+str1 = malloc(sizeof(char) * size + 1);
+
+if (str1 == NULL)
+{
+return (NULL);
+}
+else
+{
+memcpy(str, s1, size1);
+memcpy(str + size1, s2, size2 + 1);
+}
+
+return (str1);
+}
