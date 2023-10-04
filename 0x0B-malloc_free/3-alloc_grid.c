0bcef927daf897d4997258925cde8193f90412d2
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-10-04 14:07:07 -0700

    Create 2D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,6 @@ return (NULL);
 grid = (int **) malloc(height * sizeof(int *));
 if (grid == NULL)
 {
-free(grid);
 return (NULL);
 }
 
@@ -28,7 +27,10 @@ for (i = 0; i < height; i++)
 grid[i] = (int *) malloc(width * sizeof(int));
 if (grid[i] == NULL)
 {
-free(grid[i]);
+for (j = 0; j < i; j++)
+free(grid[j]);
+
+free(grid);
 return (NULL);
 }
 }
