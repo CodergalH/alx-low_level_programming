2e963a70e626aaed0fe027b426ee0882ba6d9e69
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-10-04 14:50:15 -0700

    seperate string into words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * strtow - split string into words
+ *
+ * @str: string
+ * Return: pointer to array of strings
+ */
+
+char **strtow(char *str)
+{
+int i, j, k, len = 0;
+char **words;
+
+if (str == NULL || str[0] == '\0')
+return (NULL);
+
+for (i = 0; str[i] != '\0'; i++)
+if (str[i] == ' ')
+len++;
+
+words = malloc((len + 2) * sizeof(char *));
+if (words == NULL)
+return (NULL);
+
+words[0] = str;
+for (i = 1, j = 0; str[j] != '\0'; i++)
+{
+if (str[j] == ' ')
+{
+str[j] = '\0';
+words[i] = str + j + 1;
+j++;
+}
+else
+j++;
+}
+words[i] = NULL;
+
+return (words);
+}
