9c3cf107a656fbec5c1f160f1f45ecfb4ada165e
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-10-04 00:11:22 -0700

    concat string"

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,7 @@
 char *str_concat(char *s1, char *s2)
 {
 int size, size1, size2;
-char *str1;
+char *str;
 
 if (s1 == NULL)
 s1 = "";
@@ -32,9 +32,9 @@ size += 1;
 size2 += 1;
 }
 
-str1 = malloc(sizeof(char) * size + 1);
+str = malloc(sizeof(char) * size + 1);
 
-if (str1 == NULL)
+if (str == NULL)
 {
 return (NULL);
 }
@@ -44,5 +44,5 @@ memcpy(str, s1, size1);
 memcpy(str + size1, s2, size2 + 1);
 }
 
-return (str1);
+return (str);
 }
