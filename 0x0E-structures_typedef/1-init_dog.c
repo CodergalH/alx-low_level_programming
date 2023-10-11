fd071469ca0a643b19ec4351dc8d90d1b0331b2e
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-10-11 13:51:37 -0700

    init struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,20 +9,13 @@
  * @name: name variable
  * @age: age variable
  * @owner: owner variable
- * Return: struct d
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
-d = malloc(typeof(struct dog));
-
-if (d == NULL)
-return (NULL);
-
 d->name = name;
 d->age = age;
 d->owner = owner;
 
-return (d);
 }
