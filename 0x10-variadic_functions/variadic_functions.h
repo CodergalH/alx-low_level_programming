79aa0593d17571f399e2c0ff68114a6aeb64feec
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-10-12 14:22:45 -0700

    print strings

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,5 +7,6 @@
 int _putchar(char *s);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
 
 #endif
