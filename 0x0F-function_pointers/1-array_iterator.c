9a90bf5de851ccfbf299bcc10a03c3307629bb5f
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-10-14 10:47:59 -0700

    execute func on array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function
- * on every member of an array
- *
- * @array: array
- * @size: size of array
- * @action: function to be executed
- */
+*array_iterator - executes a function on each element of an array
+*@array: array to be targeted
+*@size: size of array
+*@action: function to be executed
+*
+*/
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-int i;
 
 if (array == NULL || action == NULL)
 return;
 
-for (i = 0; i < size; i++)
-action(array[i]);
+while (size-- > 0)
+{
+action(*array);
+array++;
+}
 
 }
---
+++ 0x0F-function_pointers/C1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function
+ * on every member of an array
+ *
+ * @array: array
+ * @size: size of array
+ * @action: function to be executed
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int i;
+
+if (array == NULL || action == NULL)
+return;
+
+for (i = 0; i < size; i++)
+action(array[i]);
+
+}
