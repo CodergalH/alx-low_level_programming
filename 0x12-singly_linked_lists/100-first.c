e628dde69a50cb4da153affadaeeef410288905c
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-10-20 04:07:55 -0700

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * before_main - execute before main function
+ *
+ */
+
+void before_main(void)
+{
+printf("You're beat! and yet, you must allow,\n
+I bore my house upon my back!\n");
+
+return (0);
+}
