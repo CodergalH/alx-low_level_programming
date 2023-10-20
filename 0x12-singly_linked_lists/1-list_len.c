094fecb3d3a77f8d7d99cf78d06705779b50f919
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-10-19 21:36:06 -0700

    list, add, free node

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - finds the number of elements in a linked list
+ *@h: pointer to the list
+ *
+ *Return: the number of elements
+*/
+
+size_t list_len(const list_t *h)
+	{
+		size_t nodes = 0;
+
+		while (h)
+		{
+			nodes++;
+			h = h->next;
+		}
+
+		return (nodes);
+	}
