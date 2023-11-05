7d52a433e759097d0d2d8679743714c28a6af0e9
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-11-05 09:13:42 -0800

    0x14

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * get_endianness - get order of arrangement of bytes
+ * Return: 1 - for little endian
+ * 0 - big endian
+ */
 int get_endianness(void)
 {
 unsigned int value = 1;
