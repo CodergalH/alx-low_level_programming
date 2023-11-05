096506901cd562d4ffd0536d99c48a5a907c7076
Ohwojeheri Glorious <Ohwojeheriglorious608@gmail.com>
2023-11-05 09:07:51 -0800

    0x14

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
-int get_endianness(void) {
+int get_endianness(void)
+{
 unsigned int value = 1;
 unsigned char *byte = (unsigned char *)&value;
 
 if (*byte == 1)
-return 1;
+return (1);
 else
-return 0;
+return (0);
 }
