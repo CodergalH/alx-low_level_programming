8643a26909882be303ac84f92bb921474f8d9a7b
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-02-09 04:21:40 -0800

    task 2.10

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index.
+ * @n: integer
+ * @index: index of bit to be returned
+ *
+ * Return: value of bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int num, i, bit_index;
+	int binary[INT_SIZE];
+
+	num = INT_SIZE - 1;
+	bit_index = num - index;
+
+	while (num >= 0)
+	{
+		binary[num] = n & 1;
+		num--;
+		n >>= 1;
+	}
+
+	if (bit_index > INT_SIZE)
+		return (-1);
+	else
+		return (binary[bit_index]);
+}
+
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define INT_SIZE sizeof(int) * 8
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +11,7 @@
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif /* MAIN_H */
 
