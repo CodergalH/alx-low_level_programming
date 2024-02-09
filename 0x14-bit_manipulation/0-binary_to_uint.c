cfd664c4b52ce04a7abd017297bb88de75d4c0e9
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-02-09 07:07:12 -0800

    task 0.0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,7 +12,6 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i, j, store;
-	unsigned int dec[sizeof(int) * 64];
 	unsigned int results = 0;
 	unsigned int binary_digit[] = {0, 1};
 
@@ -21,11 +20,12 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; ++i)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] != "'0' && b[i] != '1')
 			return (0);
 	}
 
 	store = i;
+	unsigned int dec[i];
 
 	for (--i; i >= 0; --i)
 	{
