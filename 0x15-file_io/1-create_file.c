a8c365e3907fa99f2e585a71070f7984fd8d4008
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-02-12 18:57:14 -0800

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,9 +12,12 @@ int create_file(const char *filename, char *text_content)
 {
 	int created_file, i;
 
+	if (filename == NULL)
+		return (-1);
+
 	created_file = open(filename, O_RDWR | O_CREAT | O_EXCL | O_TRUNC,
 			S_IRUSR | S_IWUSR);
-	if (created_file == -1 || filename == NULL)
+	if (created_file == -1)
 		return (-1);
 
 	if (text_content == NULL)
