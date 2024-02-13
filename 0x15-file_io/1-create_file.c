a145d462d2393ffc3407846a4d3d6cdebdb31f19
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-02-12 18:49:55 -0800

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int created_file, written_bytes, i;
+	int created_file, i;
 
 	created_file = open(filename, O_RDWR | O_CREAT | O_EXCL | O_TRUNC,
 			S_IRUSR | S_IWUSR);
@@ -23,9 +23,7 @@ int create_file(const char *filename, char *text_content)
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
 
-	written_bytes = write(created_file, text_content, i);
-	if (written_bytes == -1 || written_bytes < i)
-		return (-1);
+	write(created_file, text_content, i);
 
 	close(created_file);
 	return (1);
