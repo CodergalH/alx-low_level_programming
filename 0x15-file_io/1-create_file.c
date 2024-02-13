d46d91813542697a52c606b302f0daf6ad9e8d8e
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-02-12 18:43:28 -0800

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,15 +21,9 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	if (text_content == NULL)
-	{
-		return (1);
-		exit(0);
-	}
-	else
-	{
-		for (i = 0; text_content[i] != '\0'; i++)
-			;
-	}
+		text_content = "";
+
+	for (i = 0; text_content[i] != '\0'; i++);
 
 	written_bytes = write(created_file, text_content, i);
 	if (written_bytes == -1)
